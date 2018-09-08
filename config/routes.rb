f9ccbf2eb7874d98ea9b3915ad87acf01484a0a2
Max Gudmundson <maxgud@gmail.com>
2018-09-08 13:19:35 -0600

    updated posts controller with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'users#index'
+root 'posts#index'
 resources :users do 
 	resources :posts
 end
