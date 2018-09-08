afe778586f92222636ae9eb76e25d60811c5e514
Max Gudmundson <maxgud@gmail.com>
2018-09-08 10:26:42 -0600

    controlers and models

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
