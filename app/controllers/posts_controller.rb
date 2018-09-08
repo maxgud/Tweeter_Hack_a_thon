140a95568d82d8a4d2efa8916ff4867a70f0a9a3
Max Gudmundson <maxgud@gmail.com>
2018-09-08 15:27:55 -0600

    Kinda whatever.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,44 @@
 class PostsController < ApplicationController
   before_action :authenticate_profile!
-
-
+ 
   def index
+
   end
 
   def show
   end
 
   def new
+    @post = @user.posts.new
+    render partial: "form"
   end
 
   def edit
+    render partial: "form"
   end
 
   def create
+    @post = @user.posts.new(post_params)
+    if @post.save
+      redirect_to [@user, @post]
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to [@user, @post]
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to user_posts_path
   end
+
+
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
+ 	has_many :comments
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
